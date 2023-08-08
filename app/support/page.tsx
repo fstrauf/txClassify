f2911e71442cb2ff2da78453982e1a7fc671efc2
fstrauf <f.strauf@gmail.com>
2023-08-09 09:24:48 +1000

    update page and images

---
+++ app/support/page.tsx
@@ -0,0 +1,12 @@
+export default function ContactUs() {
+  return (
+    <>
+      <div className="flex justify-center items-center mt-10">
+        <p className="mb-4">
+          If you need any help,
+          please contact us at <a href="mailto:f.strauf@gmail.com" className="underline">here</a>.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+// components/Footer.js
+
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-[#1E1E1E] text-white p-4">
+      <div className="flex space-x-4">
+        <Link href="/privacy-policy" className="hover:underline text-xs">
+          Privacy Policy
+        </Link>
+        <Link href="/support" className="hover:underline text-xs">
+          Support
+        </Link>
+        <Link href="/terms-of-service" className="hover:underline text-xs">
+          Terms of Service
+        </Link>
+      </div>
+    </footer>
+  );
+}
