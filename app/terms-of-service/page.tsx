753e4a45982f089e2a504270e7afe078cc525885
fstrauf <f.strauf@gmail.com>
2023-08-09 09:24:48 +1000

    update page and images

---
+++ app/terms-of-service/page.tsx
@@ -0,0 +1,58 @@
+export default function TermsOfService() {
+  return (
+    <>
+      <div className="min-h-screen prose flex flex-col m-auto">
+        <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+          <h1 className="mb-8 text-3xl font-bold">Terms of Service</h1>
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-xl mt-4">1. Introduction</h2>
+            <p>
+              Welcome to Expense Sorter. These are the Terms and Conditions
+              governing your use of Expense Sorter and the products,
+              services, and content offered through it.
+            </p>
+
+            <h2 className="text-xl mt-4">2. Changes to Terms</h2>
+            <p>
+              We reserve the right to modify or replace these Terms at any time.
+              It's your responsibility to check the Terms periodically for
+              changes.
+            </p>
+
+            <h2 className="text-xl mt-4">3. Access and Use</h2>
+            <p>
+              You are responsible for any activity that occurs through your
+              account. You agree not to access, copy, or otherwise use Expense Sorter, including our intellectual property and
+              trademarks, except as authorized by these Terms or as otherwise
+              authorized in writing by Expense Sorter.
+            </p>
+
+            <h2 className="text-xl mt-4">4. Limitation of Liability</h2>
+            <p>
+              To the maximum extent permitted by law, Expense Sorter
+              assumes no liability or responsibility for any errors, mistakes,
+              or inaccuracies of content; personal injury or property damage
+              resulting from your access to or use of our service; unauthorized
+              access to or use of our servers and/or any personal information
+              stored therein.
+            </p>
+
+            <h2 className="text-xl mt-4">5. Governing Law</h2>
+            <p>
+              These Terms shall be governed by the laws of Singapore, without respect to its conflict of laws
+              principles.
+            </p>
+
+            <h2 className="text-xl mt-4">6. Feedback</h2>
+            <p>
+              We appreciate feedback, comments, ideas, proposals, and
+              suggestions for improvements to Expense Sorter
+              ("Feedback"). If you choose to submit Feedback, you agree that we
+              are free to use it without any obligation to you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,22 @@
+// components/Header.js
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-[#1E1E1E] text-white p-4 flex items-center">
+      <div className="relative">
+        <Image
+          width={32}
+          height={32}
+          src="/128_logo_es.png"
+          className="rounded-md shadow-xl"
+          alt="Token Editor Flow"
+        />
+      </div>
+      <h1 className="ml-3 text-xl">
+        <Link href="/">Expense Sorter</Link>
+      </h1>
+    </header>
+  );
+}
