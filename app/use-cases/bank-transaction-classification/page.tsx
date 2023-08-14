43120935c28518a6c9d6e71ff43477339d1b97f7
fstrauf <f.strauf@gmail.com>
2023-08-14 19:27:33 +1000

    seo pages

---
+++ app/use-cases/bank-transaction-classification/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+export default function BankTransactionClassification() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gradient-to-br from-first via-second to-third">
+
+      <main className="flex-grow flex items-center justify-center p-10">
+        <div className="w-full max-w-4xl bg-third p-6 rounded-xl shadow-lg text-white space-y-6">
+          <h1 className="text-3xl font-bold leading-tight text-center">
+            Mastering Bank Transaction Classification
+          </h1>
+          <p className="text-lg text-center">
+            As the digital financial landscape expands, the need for organized bank transactions has never been more paramount. Bank transaction classification stands as the linchpin for effective financial management and clarity.
+          </p>
+          <p className="text-lg text-center">
+            By classifying bank transactions effectively, individuals and businesses can effortlessly track expenses, monitor revenue streams, and maintain robust financial health.
+          </p>
+          <h2 className="text-2xl text-first text-center mt-4">
+            Elevate Financial Oversight with Precision
+          </h2>
+          <p className="text-lg text-center">
+            Our solution offers unparalleled bank transaction classification, utilizing cutting-edge algorithms and user-friendly interfaces. Seamlessly categorize and analyze your banking activities, and lay the foundation for proactive financial decisions.
+          </p>
+          <div className="mt-6 text-center">
+            <Link href="/" className="bg-first hover:bg-second py-2 px-6 rounded-full text-white font-semibold transition duration-300 ease-in-out">
+                Delve into Classification
+            </Link>
+          </div>
+        </div>
+      </main>
+
+    </div>
+  );
+}
---
+++ app/use-cases/expense-classification/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+export default function ExpenseClassification() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gradient-to-br from-first via-second to-third">
+
+
+      <main className="flex-grow flex items-center justify-center p-10">
+        <div className="w-full max-w-4xl bg-third p-6 rounded-xl shadow-lg text-white space-y-6">
+          <h1 className="text-3xl font-bold leading-tight text-center">
+            The Significance of Expense Classification
+          </h1>
+          <p className="text-lg text-center">
+            Expense classification isn't just about keeping your bank transactions tidy. It's a crucial aspect of financial planning, budgeting, and effective money management.
+          </p>
+          <p className="text-lg text-center">
+            Through accurate classification, you can identify spending patterns, make informed financial decisions, and stay on top of your financial goals.
+          </p>
+          <h2 className="text-2xl text-first text-center mt-4">
+            Simplify with AI-powered Classification
+          </h2>
+          <p className="text-lg text-center">
+            Using our AI-driven tool, you can seamlessly integrate expense classification into your daily routine. No more manual categorization. Just link it to your Google Sheet, and let the technology handle the rest.
+          </p>
+          <div className="mt-6 text-center">
+            <Link href="/" className="bg-first hover:bg-second py-2 px-6 rounded-full text-white font-semibold transition duration-300 ease-in-out">
+                Explore the Tool
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
