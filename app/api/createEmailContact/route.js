1ac7a0e3aad727f87facbf2d00aa0914c41d23b3
fstrauf <f.strauf@gmail.com>
2023-08-25 07:38:32 +1000

    updated API call

--- app/api/createEmailContact/route.js
+++ app/api/createEmailContact/route.js
@@ -3,28 +3,27 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   const data = await req.json();
 
-  const email = data.email
+  const email = data.email;
 
   try {
-    // Create a new contact in SendFox using the Fetch API
     const response = await fetch("https://api.sendfox.com/contacts", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.SENDFOX_API_KEY}`,
       },
-      body: JSON.stringify({ email, lists: ["441356"] }), // You can add more properties if needed
+      body: JSON.stringify({ email, lists: ["441356"] }),
     });
 
     if (response.ok) {
-      return NextResponse.json(response.json());
+      const jsonResponse = await response.json();
+      return NextResponse.json(jsonResponse);
     } else {
       console.error("Error creating contact:", response.statusText);
+      return NextResponse.error("Error creating contact");
     }
   } catch (error) {
     console.error("Error submitting email:", error);
     return NextResponse.error("Error submitting email");
   }
-
 }
-
