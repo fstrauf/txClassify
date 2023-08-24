a100f9728646c7a0d57718e9319ec9d988b5ca16
fstrauf <f.strauf@gmail.com>
2023-08-24 17:01:05 +1000

    try meta properties

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Expense Sorted App',
+    short_name: 'Expense Sorted App',
+    description: 'Automatically categorise your monthly expenses using AI. Hook this App up to your Google Sheet and get your monthly budgeting done in no time.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1E1E1E',
+    theme_color: '#9C59EE',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
