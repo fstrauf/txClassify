1277afbbb5b0ff5bdb24389b2c25caf20930d006
fstrauf <f.strauf@gmail.com>
2023-08-31 07:29:13 +1000

    path redirect to current page after login

--- components/buttons/login-button.js
+++ components/buttons/login-button.js
@@ -1,10 +1,12 @@
+import { usePathname } from 'next/navigation';
+
 export const LoginButton = () => {
-  const currentUrl = typeof window !== 'undefined' ? encodeURIComponent(window.location.pathname) : '';
+  const pathname = usePathname()
 
   return (
     <a
       className="bg-first hover:bg-second py-2 px-6 rounded-full text-white font-semibold transition duration-300 ease-in-out inline-block"
-      href={`/api/auth/login?returnTo=${currentUrl}`}
+      href={`/api/auth/login?returnTo=${pathname}`}
     >
       Log In
     </a>
