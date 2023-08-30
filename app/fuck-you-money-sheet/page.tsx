5f3a28a582bb97cedd99a4c39ee726d5d5804f48
fstrauf <f.strauf@gmail.com>
2023-08-30 20:52:42 +1000

    cleanups and error handling

--- app/fuck-you-money-sheet/page.tsx
+++ app/fuck-you-money-sheet/page.tsx
@@ -5,10 +5,7 @@ import GetItHereButton from "./getItHereButton";
 import Instructions from "./instructions";
 import Intro from "./intro";
 
-
 export default function FuckYouMoneySheet() {
-
-
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-br from-first via-second to-third">
       <main className="flex-grow flex items-center justify-center p-10">
@@ -17,34 +14,46 @@ export default function FuckYouMoneySheet() {
             Fuck you Money - Cost of Living Tracking Google Sheet
           </h1>
           <GetItHereButton />
-          <div className="mt-6">
-            <Image
-              width={3214 / 2}
-              height={1356 / 2}
-              src="/f-you-money-overview.png"
-              className="rounded-md"
-              alt="Expense Sorter"
-              priority={true}
-            />
-          </div>
-          <nav className="flex space-x-4">
-            <Link
-              className="bg-second hover:bg-third py-2 px-4 rounded-full text-white font-semibold transition duration-300 ease-in-out no-underline"
-              href="#intro"
-            >
-              Intro
-            </Link>
-            <Link
-              className="bg-second hover:bg-third py-2 px-4 rounded-full text-white font-semibold transition duration-300 ease-in-out no-underline"
-              href="#instructions"
-            >
-              Instructions
-            </Link>
-          </nav>
+          <ImageComponent />
+          <Navigation />
           <Intro />
           <Instructions />
         </div>
       </main>
     </div>
+  );
+}
+
+function ImageComponent() {
+  return (
+    <div className="mt-6">
+      <Image
+        width={3214 / 2}
+        height={1356 / 2}
+        src="/f-you-money-overview.png"
+        className="rounded-md"
+        alt="Expense Sorter"
+        priority={true}
+      />
+    </div>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav className="flex space-x-4">
+      <Link
+        className="bg-second hover:bg-third py-2 px-4 rounded-full text-white font-semibold transition duration-300 ease-in-out no-underline"
+        href="#intro"
+      >
+        Intro
+      </Link>
+      <Link
+        className="bg-second hover:bg-third py-2 px-4 rounded-full text-white font-semibold transition duration-300 ease-in-out no-underline"
+        href="#instructions"
+      >
+        Instructions
+      </Link>
+    </nav>
   );
 }
