bc7e0659943a7e489fd96fd01d387f58c4ce60ca
fstrauf <f.strauf@gmail.com>
2023-08-31 12:37:22 +1000

    profile page icon

--- components/buttons/logout-button.js
+++ components/buttons/logout-button.js
@@ -1,7 +1,7 @@
 export const LogoutButton = () => {
   return (
     <a
-      className="bg-first hover:bg-second py-1 px-3 rounded-full text-white font-semibold transition duration-300 ease-in-out inline-block"
+      className="bg-first hover:bg-second py-1 px-3 rounded-full text-white font-semibold transition duration-300 ease-in-out flex items-center justify-center"
       href="/api/auth/logout"
     >
       Log Out
