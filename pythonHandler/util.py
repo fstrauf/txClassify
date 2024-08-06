afd24958ecfc812965f2abf6696dd8428de913e8
fstrauf <f.strauf@gmail.com>
2024-08-06 10:41:15 +0300

    update

--- pythonHandler/util.py
+++ pythonHandler/util.py
@@ -1,9 +1,2 @@
-from urllib.parse import urlparse
 
-def is_valid_https_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme == 'https', result.netloc, result.path])
-    except:
-        return False
     
