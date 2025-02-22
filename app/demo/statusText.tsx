31daa63c0615c7de90ba46ef68c32dc047acbbb3
fstrauf <f.strauf@gmail.com>
2025-02-23 12:33:14 +1300

    remove demo page

--- app/demo/statusText.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface StatusTextProps {
-  text: string;
-}
-
-export default function StatusText({ text }: StatusTextProps) {
-  return <p className="text-sm">{text}</p>;
-}
