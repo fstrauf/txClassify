c1b6f06f189acddc8c8d3f6bf18b3cad2999e865
fstrauf <f.strauf@gmail.com>
2025-02-24 14:18:26 +1300

    posthog app

--- app/components/PostHogProvider.tsx
+++ app/components/PostHogProvider.tsx
@@ -3,37 +3,28 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import PostHogPageView from "./PostHogPageView"
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug()
-    },
-    capture_pageview: false // We'll handle this manually
-  })
-}
-
-export default function PHProvider({
+export default function PostHogProviderWrapper({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
   useEffect(() => {
-    if (pathname) {
-      let url = window.origin + pathname
-      if (searchParams?.toString()) {
-        url = url + `?${searchParams.toString()}`
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      person_profiles: 'identified_only', // Only create profiles for identified users
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
       }
-      posthog.capture('$pageview', {
-        $current_url: url,
-      })
-    }
-  }, [pathname, searchParams])
+    })
+  }, [])
 
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  return (
+    <PostHogProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PostHogProvider>
+  )
 } 
