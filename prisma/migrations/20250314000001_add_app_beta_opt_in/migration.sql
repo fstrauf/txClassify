b606b2a6bd9fd3a1125a12825c2c56117ec27e40
fstrauf <f.strauf@gmail.com>
2025-03-28 21:12:26 +1300

    remove fe code

--- prisma/migrations/20250314000001_add_app_beta_opt_in/migration.sql
+++
@@ -1,5 +0,0 @@
--- Create the enum type
-CREATE TYPE "AppBetaOptInStatus" AS ENUM ('OPTED_IN', 'DISMISSED');
--- Add the column to the account table
-ALTER TABLE "account"
-ADD COLUMN "appBetaOptIn" "AppBetaOptInStatus";
